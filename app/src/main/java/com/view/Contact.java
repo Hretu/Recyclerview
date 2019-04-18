cfe906fd39b8f7897bebbdda54b84dc868ace916
Hretu <ritudhakal5@gmail.com>
2019-04-18 09:43:15 +0545

    Initial commit topic 5 recycler view

---
+++ app/src/main/java/com/view/Contact.java
@@ -0,0 +1,46 @@
+package com.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Contact extends AppCompatActivity {
+    private String name;
+    private String phoneNo;
+    private int imageID;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNo() {
+        return phoneNo;
+    }
+
+    public void setPhoneNo(String phoneNo) {
+        this.phoneNo = phoneNo;
+    }
+
+    public int getImageID() {
+        return imageID;
+    }
+
+    public void setImageID(int imageID) {
+        this.imageID = imageID;
+    }
+
+    public Contact(String name, String phoneNo, int imageID) {
+        this.name = name;
+        this.phoneNo = phoneNo;
+        this.imageID = imageID;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact);
+    }
+}
